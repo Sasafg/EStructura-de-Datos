d8c14540275b8b6bc4d2444a37f6bb8d59b56efc
Sasafg <saryfg@gmail.com>
2022-09-29 08:05:13 -0600

    practica
    a

---
+++ EstructuraViernes/src/main/java/Tareas/Recursion.java
@@ -0,0 +1,24 @@
+
+package Tareas;
+
+/**
+ *
+ * @author Saryf
+ */
+public class Recursion {
+    
+    
+    
+     static int arr []- {12,34,64,2,3}
+     
+     public static int factorial(int n){
+         if (n==0){
+             return 1;
+         } else {
+                     System.out.print.ln(n);
+                     return n * factorial(n-1);
+                     
+         
+         }
+     }
+}
